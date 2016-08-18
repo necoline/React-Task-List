9bcfd540565b0432a669adddcae18f4070c3b497
necoline <hnhubner@gmail.com>
2016-08-17 20:56:59 -0600

    first day

--- taskList/src/App.js
+++ taskList/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TaskListApp from './components/TaskListApp';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>The Relationship App</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TaskListApp />
       </div>
     );
   }
